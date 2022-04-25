b9975c36dc89ba37b7974de9467f5e9dfd3550a1
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-04-25 18:18:32 +0200

    from scratch the beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import { Button, styled, Typography } from '@mui/material';
-import './App.css'
-import { Add, Settings } from '@mui/icons-material';
-
 function App() {
   
-  // styled component mui
-    const BlueButton = styled(Button)(({theme})=>({
-      backgroundColor: theme.palette.otherColor,
-      color: "#888",
-      margin: 5,
-      "&:hover": {
-        backgroundColor: "lightblue"
-      },
-      "&:disabled": {
-        backgroundColor: "gray",
-        color: "white",
-      }
-    }));
-
   return (
     <div className="App">
-      I hope to be integrated in On Pace please \o/
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={ <Settings /> }
-        variant="contained"
-        color="secondary"
-        size="small"
-        >
-         Settings
-         </Button>
-      <Button color="success" startIcon={ <Add /> } variant="outlined">Add new post</Button>
-      <Typography variant="h1" component="h2">
-        h1. Heading
-      </Typography>
-      <BlueButton>
-        My button
-      </BlueButton>
+ 
     </div>
   )
 }
