6a55ed59d1f0ea3e2fbdf3428dba96810f2da46a
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-04-25 18:14:39 +0200

    MUI started, to learn before beginning

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    // where we store colors
+    palette: {
+        primary: {
+            main: "#1760a5",
+            light: "skyblue",
+        },
+
+        secondary: {
+            main: "#15c630",
+        },
+
+        otherColor: {
+            main: "#999",
+        },
+    }
+})
